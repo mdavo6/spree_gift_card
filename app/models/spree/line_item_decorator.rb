01885865bbf458b30ec7fd8f7aee72213f96f18d
Michael Davidson <mdavo6@gmail.com>
2021-05-04 14:17:18 +1000

    Move with_options block inside self.prepended

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -3,16 +3,15 @@ module Spree
 
     def self.prepended(base)
       base.has_one :gift_card, dependent: :destroy
+      with_options if: :is_gift_card? do
+        base.validates :gift_card, presence: true
+        base.validates :quantity,  numericality: { less_than_or_equal_to: MAXIMUM_GIFT_CARD_LIMIT }, allow_nil: true
+      end
+
+      base.delegate :is_gift_card?, to: :product
     end
 
     MAXIMUM_GIFT_CARD_LIMIT = 1
-
-    with_options if: :is_gift_card? do
-      validates :gift_card, presence: true
-      validates :quantity,  numericality: { less_than_or_equal_to: MAXIMUM_GIFT_CARD_LIMIT }, allow_nil: true
-    end
-
-    delegate :is_gift_card?, to: :product
 
   end
 end
