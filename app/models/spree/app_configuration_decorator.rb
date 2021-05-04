41f2883c1502f7707fef0e0a9002731633dc99a3
Michael Davidson <mdavo6@gmail.com>
2021-05-04 13:43:02 +1000

    Fix syntax error

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -2,9 +2,9 @@ module Spree
   module AppConfigurationDecorator
 
     def self.prepended(base)
-      preference :allow_gift_card_redeem, :boolean, default: true
+      base.preference :allow_gift_card_redeem, :boolean, default: true
     end
-    
+
   end
 end
 
