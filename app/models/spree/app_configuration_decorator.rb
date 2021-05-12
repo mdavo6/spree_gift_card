d7aa2d30714bfdd0286f0ef1a63b098dbe884100
Michael Davidson <mdavo6@gmail.com>
2021-05-12 12:21:46 +1000

    Change app configuration from prepend to include

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,11 +1,11 @@
 module Spree
   module AppConfigurationDecorator
 
-    def self.prepended(base)
+    def self.included(base)
       base.preference :allow_gift_card_redeem, :boolean, default: true
     end
 
   end
 end
 
-::Spree::AppConfiguration.prepend(Spree::AppConfigurationDecorator)
+::Spree::AppConfiguration.include(Spree::AppConfigurationDecorator)
